73812811843cce6faced0bc6c9ffd18cf5da5503
1747763533@qq.com <1747763533@qq.com>
2021-08-10 16:36:38 +0800

    push commit

--- src/main/java/com/wdh/git/GitTest.java
+++ src/main/java/com/wdh/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello git4!");
         System.out.println("Hello test !");
         System.out.println("Hello git5!");
+        System.out.println("push git5!");
     }
 
 }

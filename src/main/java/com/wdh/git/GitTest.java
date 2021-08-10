5e26596c7cbc6d8de6bcbfcb80ad0093fede908d
1747763533@qq.com <1747763533@qq.com>
2021-08-10 16:01:53 +0800

    frist commit

--- src/main/java/com/wdh/git/GitTest.java
+++ src/main/java/com/wdh/git/GitTest.java
@@ -2,7 +2,8 @@ package com.wdh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello git!");
+        System.out.println("Hello git1!");
+        System.out.println("Hello git2!");
     }
 
 }

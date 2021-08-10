ff697009f5f79afe83726a4ca8ac0490ef63ee8e
1747763533 <58960512+1747763533@users.noreply.github.com>
2021-08-10 16:43:27 +0800

    pull commit

--- src/main/java/com/wdh/git/GitTest.java
+++ src/main/java/com/wdh/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("Hello test !");
         System.out.println("Hello git5!");
         System.out.println("push git5!");
+        System.out.println("pull git5!");
     }
 
 }
